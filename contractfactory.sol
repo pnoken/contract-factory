98c751f6b4f2b2e16a3d6f6063dd514de031bc1e
pnoken <ecexpay@gmail.com>
2023-09-12 17:49:13 +0000

    initial commit

---
+++ contractfactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract VotingFactory {
+    address[] public votingContracts;
+
+    event VotingContractCreated(
+        address indexed votingContract,
+        address indexed creator
+    );
+
+    function createVotingContract() public {
+        Voting newVoting = new Voting(msg.sender);
+        votingContracts.push(address(newVoting));
+        emit VotingContractCreated(address(newVoting), msg.sender);
+    }
+
+    function getVotingContracts() public view returns (address[] memory) {
+        return votingContracts;
+    }
+}
